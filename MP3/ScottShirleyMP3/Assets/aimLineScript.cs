4917e2e1112d95fc55151b59b823d15822fd26fc
Scottin3d <scottin3d@gmail.com>
2020-10-22 19:43:37 -0700

    MP3 Start

---
+++ MP3/ScottShirleyMP3/Assets/aimLineScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class aimLineScript : MonoBehaviour
+{
+    [SerializeField]
+    Transform p1;
+
+    [SerializeField]
+    Transform p2;
+
+    [SerializeField]
+    float lineWidth = 1;
+
+    private void Start() {
+    }
+
+    private void Update() {
+        Vector3 V = p2.localPosition - p1.localPosition;
+        float length = V.magnitude;
+
+        transform.localPosition = p1.localPosition + 0.5f * V;
+        transform.localScale = new Vector3(lineWidth, length * 0.5f, lineWidth);
+
+        transform.localRotation = Quaternion.FromToRotation(Vector3.up, V);
+    }
+}
---
+++ MP3/ScottShirleyMP3/Assets/endPointScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+
+public class endPointScript : MonoBehaviour
+{
+    private Vector3 mOffset;
+    private float mZCoord;
+    Vector3 mousePos;
+
+    [SerializeField]
+    GameObject plane;
+
+    GameObject target;
+    bool selected = false;
+
+    void Update() {
+        //if click,
+        if (Input.GetMouseButton(0)) {
+            click();
+        }
+
+        if (Input.GetMouseButtonUp(0)) {
+            selected = false;
+        }
+    }
+
+    void click() {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (!selected) {
+            if (Physics.Raycast(ray, out hit)) {
+                // plane? move object
+                if (hit.collider.CompareTag("point")) {
+                    target = hit.collider.gameObject;
+                    Debug.Log(target);
+                    selected = true;
+                }
+            }
+        } else {
+            if (Physics.Raycast(ray, out hit)){
+                if (hit.collider.gameObject == plane) {
+                    target.transform.position = hit.point;
+                }
+            }
+        }
+
+
+    }
+
+}
