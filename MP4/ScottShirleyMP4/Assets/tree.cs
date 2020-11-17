54fddc278510b02e115c93d55d10ee24859e66ef
Scottin3d <scottin3d@gmail.com>
2020-11-16 19:47:23 -0800

    trees

---
+++ MP4/ScottShirleyMP4/Assets/tree.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tree : MonoBehaviour
+{
+    public Color treeColor = Color.white;
+    MeshRenderer mr;
+    private void Start() {
+        mr = GetComponent<MeshRenderer>();
+        mr.material = Instantiate<Material>(mr.material);
+    }
+
+    private void Update() {
+        mr.material.color = treeColor;
+    }
+}
