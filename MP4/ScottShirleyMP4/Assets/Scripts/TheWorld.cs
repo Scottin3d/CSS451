d6edef9137d4c91bed15addbdedb20354d1a76bf
Scottin3d <scottin3d@gmail.com>
2020-11-20 15:24:17 -0800

    final changes

--- MP4/ScottShirleyMP4/Assets/Scripts/TheWorld.cs
+++ MP4/ScottShirleyMP4/Assets/Scripts/TheWorld.cs
@@ -8,14 +8,31 @@ public class TheWorld : MonoBehaviour
 {
     public SceneNode TheRoot;
 
+    bool isPause = false;
+    int timeScale = 1;
+
+    void Start() {
+        
+    }
+
     // Update is called once per frame
     void Update()
     {
+        Time.timeScale = timeScale;
         Matrix4x4 i = Matrix4x4.identity;
         TheRoot.CompositeXform(ref i);
 
         if (Input.GetKeyDown(KeyCode.H)) {
             SceneManager.LoadScene("ScottShirleyMP4");
+        }
+
+        if (Input.GetKeyDown(KeyCode.P)) {
+            timeScale = (isPause) ? 1 : 0;
+            isPause = !isPause;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            Application.Quit();
         }
     }
 }
--- MP4/ScottShirleyMP4/Assets/miniCameraScript.cs
+++ MP4/ScottShirleyMP4/Assets/miniCameraScript.cs
@@ -10,6 +10,7 @@ public class miniCameraScript : MonoBehaviour
     public Vector3 miniCamPos = Vector3.zero;
 
     public GameObject root = null;
+    public GameObject lookAtLine = null;
     // Start is called before the first frame update
     void Start()
     {
@@ -26,6 +27,11 @@ public class miniCameraScript : MonoBehaviour
         transform.position = miniCamPos;
         transform.rotation = Quaternion.FromToRotation(Vector3.up, miniCamT.up);
 
+        Vector3 dir = transform.forward;
+        Vector3 pos = dir * 15f + transform.position;
+        lookAt.position = pos;
+
+        AdjustLine(lookAtLine,transform.position, lookAt.position);
     }
 
     private Transform TreeTip(Transform t) {
@@ -33,5 +39,13 @@ public class miniCameraScript : MonoBehaviour
             return t;
         }
         return TreeTip(t.GetChild(0));
+    }
+
+    void AdjustLine(GameObject line, Vector3 p1, Vector3 p2, float lineWidth = 0.1f) {
+        Vector3 V = p2 - p1;
+        float length = V.magnitude;
+        line.transform.localPosition = p1 + 0.5f * V;
+        line.transform.localScale = new Vector3(lineWidth, length * 0.5f, lineWidth);
+        line.transform.localRotation = Quaternion.FromToRotation(Vector3.up, V);
     }
 }
